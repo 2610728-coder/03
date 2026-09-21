41c2e1470f87180b3d4ca60123ce0419cb030f26
2610728-coder <2610728@sookmyung.ac.kr>
2026-09-21 11:21:40 +0900

    04

--- main.c
+++ main.c
@@ -2,15 +2,18 @@
 
 int main(void)
 {
-    char c;
+    int a, b;
     //int c1;
     
-    printf("input a character :");
-    scanf("%c", &c);
+    printf("input a number :");
+    scanf("%d", &a);
+
+    printf("input a number :");
+    scanf("%d", &b);
 
     //c1 = c + 1;
 
-    printf("The input character of %c (%d) is %c (%d)\n", c, c, c+1, c+1);
+    printf("Dividing result is %f\n", (float)a / b);
 
     return 0;
 }
